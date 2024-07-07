3c04c789b41d2cc36974ac2973bddbfe6464c790
Pravin Kumar <pravinkumar.shanmugasundaram@zf.com>
2024-07-07 12:22:30 +0530

    basic styles added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import Navigation from './Navigation'
-import Logo from './Logo'
+
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center p-8'>
-        <Logo text='Pravin Kumar'/>
-        <Navigation/>
+    <header className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24'>
+      <div>
+        <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>
+          <a href='/'>Pravin Kumar</a>
+        </h1>
+        <h2 className='mt-3 text-lg  font-medium  tracking-tight text-slate-200 sm:text-xl'>Senior Software Engineer</h2>
+        <p className='mt-4 max-w-xs leading-normal'>I build web services that are efficient, scalable and maintainable</p>
+        <nav className='nav hidden lg:block'>
+        <ul className='mt-16 w-max'>
+        <li>
+          <a href='#about'><span>About</span></a>
+        </li>
+        <li>
+          <a href='#experience'><span>Experience</span></a>
+        </li>
+        <li>
+          <a href='#projects'><span>Projects</span></a>
+        </li>
+      </ul>
+      </nav>
+      </div>
     </header>
   )
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-type SectionProps = {children: React.ReactNode}
+type SectionProps = { title: string, children: React.ReactNode}
 
-const Section = ({children}:SectionProps) => {
+const Section = ({title, children}:SectionProps) => {
   return (
-    <section className='text-red-50'>{children}</section>
+    <section className="mb-16">
+      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur'>
+        <h2 className='text-sm font-bold uppercase  tracking-widest text-slate-200'>
+        {title}
+        </h2>
+      </div>
+      <div>
+      {children}
+      </div>
+    </section>
   )
 }
 
